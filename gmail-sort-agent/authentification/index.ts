5c9eea604ec499fde72aaba4e815e537048088f0
carduuui <eckstein.jakob1@gmail.com>
2026-08-16 17:31:03 +0200

    kleinen type fehler behoben und docker pfade angepasst

--- gmail-sort-agent/authentification/index.ts
+++ gmail-sort-agent/authentification/index.ts
@@ -1,47 +1,47 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import process from 'node:process';
-import {authenticate} from '@google-cloud/local-auth';
-import {google, gmail_v1} from 'googleapis';
-import type {OAuth2Client} from 'google-auth-library';
+import { authenticate } from '@google-cloud/local-auth';
+import { google, gmail_v1 } from 'googleapis';
+import type { Auth } from 'googleapis';
 
-// The scope for reading Gmail labels.
+// The scope for reading/modifying Gmail labels.
 const SCOPES: string[] = ['https://www.googleapis.com/auth/gmail.modify'];
 // The path to the credentials file.
-const CREDENTIALS_PATH: string = path.join(process.cwd(), '../../credentials.json');
+const CREDENTIALS_PATH: string = path.join(process.cwd(), '../../gmail-sort-agent/credentials.json');
 
-const TOKEN_PATH: string = path.join(process.cwd(), '../../token.json');
+const TOKEN_PATH: string = path.join(process.cwd(), '../../gmail-sort-agent/token.json');
 
 export async function getAuth(): Promise<gmail_v1.Gmail> {
 
   let auth = await loadSavedCredentials();
 
-  if(!auth){
-    auth = await authenticate({
+  if (!auth) {
+    auth = (await authenticate({
       scopes: SCOPES,
       keyfilePath: CREDENTIALS_PATH,
-    });
+    })) as unknown as Auth.OAuth2Client;
     await saveCredentials(auth);
   }
 
-  const gmail = google.gmail({version: 'v1', auth});
+  const gmail = google.gmail({ version: 'v1', auth });
 
   return gmail;
 }
 
-async function loadSavedCredentials(): Promise<OAuth2Client | null>{
-  
-  try{
+async function loadSavedCredentials(): Promise<Auth.OAuth2Client | null> {
+
+  try {
     const content = await fs.readFile(TOKEN_PATH, 'utf-8');
-    const credentials =  JSON.parse(content);
-    return google.auth.fromJSON(credentials) as OAuth2Client;
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials) as Auth.OAuth2Client;
   }
-  catch{
+  catch {
     return null;
   }
 }
 
-async function saveCredentials(client: OAuth2Client): Promise<void>{
+async function saveCredentials(client: Auth.OAuth2Client): Promise<void> {
   const content = await fs.readFile(CREDENTIALS_PATH, 'utf-8');
   const keys = JSON.parse(content);
   const key = keys.installed || keys.web;
