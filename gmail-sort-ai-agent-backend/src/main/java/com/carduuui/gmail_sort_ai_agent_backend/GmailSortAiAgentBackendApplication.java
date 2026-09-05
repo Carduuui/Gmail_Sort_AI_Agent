f1e1489b75bb0f5e85c32e0609a9033ea3c37888
carduuui <eckstein.jakob1@gmail.com>
2026-09-05 17:25:02 +0200

    springboot backend aufgebaut mit verbindugn zu postgresql db

---
+++ gmail-sort-ai-agent-backend/src/main/java/com/carduuui/gmail_sort_ai_agent_backend/GmailSortAiAgentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carduuui.gmail_sort_ai_agent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailSortAiAgentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmailSortAiAgentBackendApplication.class, args);
+	}
+
+}
---
+++ gmail-sort-ai-agent-backend/src/test/java/com/carduuui/gmail_sort_ai_agent_backend/GmailSortAiAgentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carduuui.gmail_sort_ai_agent_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailSortAiAgentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
